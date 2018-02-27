7fd4968ffca5cb49690dccfd0eb2136ce0dd9a7b
Ray <r.brown-amory@sky.com>
2018-02-27 13:57:14 +0000

    Add files via upload

---
+++ SimpleWinformApplication/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace SimpleWinformApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Namelb = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Surnamelb = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Phonelb = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Citylb = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Addresslb = new System.Windows.Forms.Label();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.addresslb5 = new System.Windows.Forms.Label();
+            this.NameLabel6 = new System.Windows.Forms.Label();
+            this.Surnamelbl7 = new System.Windows.Forms.Label();
+            this.phonelb8 = new System.Windows.Forms.Label();
+            this.citylabel9 = new System.Windows.Forms.Label();
+            this.addresslb10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Namelb
+            // 
+            this.Namelb.AutoSize = true;
+            this.Namelb.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Namelb.Location = new System.Drawing.Point(74, 76);
+            this.Namelb.Name = "Namelb";
+            this.Namelb.Size = new System.Drawing.Size(86, 31);
+            this.Namelb.TabIndex = 0;
+            this.Namelb.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(204, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Surnamelb
+            // 
+            this.Surnamelb.AutoSize = true;
+            this.Surnamelb.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Surnamelb.Location = new System.Drawing.Point(74, 139);
+            this.Surnamelb.Name = "Surnamelb";
+            this.Surnamelb.Size = new System.Drawing.Size(123, 31);
+            this.Surnamelb.TabIndex = 2;
+            this.Surnamelb.Text = "Surname";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(204, 189);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Phonelb
+            // 
+            this.Phonelb.AutoSize = true;
+            this.Phonelb.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Phonelb.Location = new System.Drawing.Point(74, 189);
+            this.Phonelb.Name = "Phonelb";
+            this.Phonelb.Size = new System.Drawing.Size(90, 31);
+            this.Phonelb.TabIndex = 4;
+            this.Phonelb.Text = "Phone";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Paris",
+            "New York",
+            "London",
+            "Sydney"});
+            this.comboBox1.Location = new System.Drawing.Point(204, 239);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Citylb
+            // 
+            this.Citylb.AutoSize = true;
+            this.Citylb.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Citylb.Location = new System.Drawing.Point(74, 239);
+            this.Citylb.Name = "Citylb";
+            this.Citylb.Size = new System.Drawing.Size(61, 31);
+            this.Citylb.TabIndex = 7;
+            this.Citylb.Text = "City";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(204, 294);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 104);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Addresslb
+            // 
+            this.Addresslb.AutoSize = true;
+            this.Addresslb.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addresslb.Location = new System.Drawing.Point(74, 294);
+            this.Addresslb.Name = "Addresslb";
+            this.Addresslb.Size = new System.Drawing.Size(112, 31);
+            this.Addresslb.TabIndex = 8;
+            this.Addresslb.Text = "Address";
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.BackColor = System.Drawing.Color.DimGray;
+            this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveBtn.Location = new System.Drawing.Point(204, 455);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(99, 42);
+            this.SaveBtn.TabIndex = 10;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = false;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.addresslb10);
+            this.panel1.Controls.Add(this.citylabel9);
+            this.panel1.Controls.Add(this.phonelb8);
+            this.panel1.Controls.Add(this.Surnamelbl7);
+            this.panel1.Controls.Add(this.NameLabel6);
+            this.panel1.Controls.Add(this.addresslb5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Location = new System.Drawing.Point(537, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 397);
+            this.panel1.TabIndex = 11;
+            this.panel1.Visible = false;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(12, 14);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(86, 31);
+            this.label.TabIndex = 1;
+            this.label.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "City";
+            // 
+            // addresslb5
+            // 
+            this.addresslb5.AutoSize = true;
+            this.addresslb5.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addresslb5.Location = new System.Drawing.Point(12, 221);
+            this.addresslb5.Name = "addresslb5";
+            this.addresslb5.Size = new System.Drawing.Size(112, 31);
+            this.addresslb5.TabIndex = 9;
+            this.addresslb5.Text = "Address";
+            // 
+            // NameLabel6
+            // 
+            this.NameLabel6.AutoSize = true;
+            this.NameLabel6.BackColor = System.Drawing.Color.Yellow;
+            this.NameLabel6.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel6.Location = new System.Drawing.Point(180, 14);
+            this.NameLabel6.Name = "NameLabel6";
+            this.NameLabel6.Size = new System.Drawing.Size(38, 25);
+            this.NameLabel6.TabIndex = 10;
+            this.NameLabel6.Text = "00";
+            // 
+            // Surnamelbl7
+            // 
+            this.Surnamelbl7.AutoSize = true;
+            this.Surnamelbl7.BackColor = System.Drawing.Color.Yellow;
+            this.Surnamelbl7.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Surnamelbl7.Location = new System.Drawing.Point(180, 66);
+            this.Surnamelbl7.Name = "Surnamelbl7";
+            this.Surnamelbl7.Size = new System.Drawing.Size(38, 25);
+            this.Surnamelbl7.TabIndex = 11;
+            this.Surnamelbl7.Text = "00";
+            // 
+            // phonelb8
+            // 
+            this.phonelb8.AutoSize = true;
+            this.phonelb8.BackColor = System.Drawing.Color.Yellow;
+            this.phonelb8.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phonelb8.Location = new System.Drawing.Point(180, 116);
+            this.phonelb8.Name = "phonelb8";
+            this.phonelb8.Size = new System.Drawing.Size(38, 25);
+            this.phonelb8.TabIndex = 12;
+            this.phonelb8.Text = "00";
+            // 
+            // citylabel9
+            // 
+            this.citylabel9.AutoSize = true;
+            this.citylabel9.BackColor = System.Drawing.Color.Yellow;
+            this.citylabel9.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.citylabel9.Location = new System.Drawing.Point(180, 167);
+            this.citylabel9.Name = "citylabel9";
+            this.citylabel9.Size = new System.Drawing.Size(38, 25);
+            this.citylabel9.TabIndex = 13;
+            this.citylabel9.Text = "00";
+            // 
+            // addresslb10
+            // 
+            this.addresslb10.AutoSize = true;
+            this.addresslb10.BackColor = System.Drawing.Color.Yellow;
+            this.addresslb10.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addresslb10.Location = new System.Drawing.Point(180, 221);
+            this.addresslb10.Name = "addresslb10";
+            this.addresslb10.Size = new System.Drawing.Size(38, 25);
+            this.addresslb10.TabIndex = 14;
+            this.addresslb10.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(826, 654);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Addresslb);
+            this.Controls.Add(this.Citylb);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Phonelb);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Surnamelb);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Namelb);
+            this.Name = "Form1";
+            this.Text = "Contact Form ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Namelb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Surnamelb;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Phonelb;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Citylb;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label Addresslb;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label addresslb10;
+        private System.Windows.Forms.Label citylabel9;
+        private System.Windows.Forms.Label phonelb8;
+        private System.Windows.Forms.Label Surnamelbl7;
+        private System.Windows.Forms.Label NameLabel6;
+        private System.Windows.Forms.Label addresslb5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ SimpleWinformApplication/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWinformApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            NameLabel6.Text = textBox1.Text;
+            Surnamelb.Text = textBox2.Text;
+            phonelb8.Text = textBox3.Text;
+            citylabel9.Text = comboBox1.Text;
+            addresslb10.Text = textBox4.Text;
+
+        }
+    }
+}
